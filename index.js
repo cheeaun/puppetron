2c10c4e1ad51dd39ce11c6752ab7cb34e4f75e21
Lim Chee Aun <cheeaun@gmail.com>
2017-08-25 12:42:45 +0800

    Upgrade puppeteer, handle tabs/windows closing, use sharp to resize

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 const http = require('http');
 const puppeteer = require('puppeteer');
+const sharp = require('sharp');
 const { URL } = require('url');
 
+let browser;
+
 require('http').createServer(async (req, res) => {
-  console.log(req.url);
   const url = req.url.replace(/^\//, '');
-  if (url && url != 'favicon.ico'){
-    try {
-      new URL(url);
-      const browser = await puppeteer.launch({args: ['--no-sandbox']});
-      const page = await browser.newPage();
-      await page.goto(url, {
-        waitUntil: 'networkidle',
-        networkIdleTimeout: 5000,
-      });
-      const screenshot = await page.screenshot({
-        type: 'jpeg',
-      });
-      browser.close();
-      res.writeHead(200, {'Content-Type': 'image/png' });
-      res.end(screenshot, 'binary');
-    } catch (e) {
-      console.error(e);
-      res.end('Oops. Invalid URL.');
-    }
-  } else {
+  if (url == 'favicon.ico'){
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+
+  if (!url){
     res.end('Append a URL pls.');
+    return;
+  }
+
+  console.log('Screenshoting ' + url);
+  try {
+    new URL(url);
+    if (!browser) {
+      browser = await puppeteer.launch({args: ['--no-sandbox']});
+    }
+    const page = await browser.newPage();
+    page.setViewport({
+      width: 1024,
+      height: 768,
+    });
+    await page.goto(url, {
+      waitUntil: 'networkidle',
+    });
+    const screenshot = await page.screenshot();
+    page.close();
+    const image = sharp(screenshot).resize(320).jpeg({
+      quality: 90,
+      progressive: true,
+    });
+    res.writeHead(200, {
+      'content-type': 'image/jpg',
+      'cache-control': 'public,max-age=31536000,immutable',
+    });
+    image.pipe(res);
+    // res.end(image, 'binary');
+  } catch (e) {
+    console.error(e);
+    res.end('Oops. Invalid URL.');
   }
 }).listen(process.env.PORT || 3000);
