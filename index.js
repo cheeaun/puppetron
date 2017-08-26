ab8637fd08dec37f16206d872efcad50a107253e
Lim Chee Aun <cheeaun@gmail.com>
2017-08-26 10:31:34 +0800

    Add PDF. Why not?

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer');
 const sharp = require('sharp');
 const LRU = require('lru-cache');
 const cache = LRU({
-  max: 15,
+  max: 15, // max 15 tabs
   maxAge: 1000 * 60, // 1 minute
   dispose: (url, page) => {
     console.log('Disposing ' + url);
@@ -35,7 +35,7 @@ require('http').createServer(async (req, res) => {
     return;
   }
 
-  const [_, action, url] = req.url.match(/^\/(screenshot|render)?\/?(.*)/i) || ['', '', ''];
+  const [_, action, url] = req.url.match(/^\/(screenshot|render|pdf)?\/?(.*)/i) || ['', '', ''];
 
   if (!url){
     res.writeHead(400, {
@@ -48,6 +48,7 @@ require('http').createServer(async (req, res) => {
   try {
     const u = new URL(url);
     const pageURL = u.origin + decodeURIComponent(u.pathname);
+    const { searchParams } = u;
     
     let page = cache.get(pageURL);
     if (!page) {
@@ -73,63 +74,80 @@ require('http').createServer(async (req, res) => {
       cache.set(pageURL, page);
     }
 
-    if (action === 'render'){
-      await page.evaluate(() => {
-        // Remove scripts except JSON-LD
-        const scripts = document.querySelectorAll('script:not([type="application/ld+json"])');
-        scripts.forEach(s => s.parentNode.removeChild(s));
+    switch (action){
+      case 'render': {
+        await page.evaluate(() => {
+          // Remove scripts except JSON-LD
+          const scripts = document.querySelectorAll('script:not([type="application/ld+json"])');
+          scripts.forEach(s => s.parentNode.removeChild(s));
 
-        // Remove import tags
-        const imports = document.querySelectorAll('link[rel=import]');
-        imports.forEach(i => i.parentNode.removeChild(i));
+          // Remove import tags
+          const imports = document.querySelectorAll('link[rel=import]');
+          imports.forEach(i => i.parentNode.removeChild(i));
 
-        // Inject <base> for loading relative resources
-        const base = document.createElement('base');
-        base.setAttribute('href', location.origin + location.pathname);
-        document.head.appendChild(base);
-      });
+          // Inject <base> for loading relative resources
+          const base = document.createElement('base');
+          base.setAttribute('href', location.origin + location.pathname);
+          document.head.appendChild(base);
+        });
 
-      let content = await page.content();
+        let content = await page.content();
 
-      // Remove comments
-      content = content.replace(/<!--[\s\S]*?-->/g, '');
+        // Remove comments
+        content = content.replace(/<!--[\s\S]*?-->/g, '');
 
-      // page.close();
-      
-      res.writeHead(200, {
-        'content-type': 'text/html; charset=UTF-8',
-        'cache-control': 'public,max-age=31536000',
-      });
-      res.end(content);
-      return;
-    }
+        res.writeHead(200, {
+          'content-type': 'text/html; charset=UTF-8',
+          'cache-control': 'public,max-age=31536000',
+        });
+        res.end(content);
+        break;
+      }
+      case 'pdf': {
+        const format = searchParams.get('format') || null;
+        const pageRanges = searchParams.get('pageRanges') || null;
+
+        const pdf = await page.pdf({
+          format,
+          pageRanges,
+        });
+        res.writeHead(200, {
+          'content-type': 'application/pdf',
+          'cache-control': 'public,max-age=31536000',
+        });
+        res.end(pdf, 'binary');
+        break;
+      }
+      default: {
+        const width = parseInt(searchParams.get('width'), 10) || 1024;
+        const height = parseInt(searchParams.get('height'), 10) || 768;
+        const thumbWidth = parseInt(searchParams.get('thumbWidth'), 10) || null;
+        const fullPage = searchParams.get('fullPage') == 'true' || false;
+        
+        page.setViewport({
+          width,
+          height,
+        });
+        const screenshot = await page.screenshot({
+          type: 'jpeg',
+          fullPage,
+        });
     
-    const width = parseInt(u.searchParams.get('width'), 10) || 1024;
-    const height = parseInt(u.searchParams.get('height'), 10) || 768;
-    const thumbWidth = parseInt(u.searchParams.get('thumbWidth'), 10) || null;
-    page.setViewport({
-      width,
-      height,
-    });
-    const screenshot = await page.screenshot({
-      type: 'jpeg',
-    });
+        res.writeHead(200, {
+          'content-type': 'image/jpg',
+          'cache-control': 'public,max-age=31536000',
+        });
 
-    // page.close();
-
-    res.writeHead(200, {
-      'content-type': 'image/jpg',
-      'cache-control': 'public,max-age=31536000,immutable',
-    });
-
-    if (thumbWidth && thumbWidth < width){
-      const image = sharp(screenshot).resize(thumbWidth).jpeg({
-        quality: 90,
-        progressive: true,
-      });
-      image.pipe(res);
-    } else {
-      res.end(screenshot, 'binary');
+        if (thumbWidth && thumbWidth < width){
+          const image = sharp(screenshot).resize(thumbWidth).jpeg({
+            quality: 90,
+            progressive: true,
+          });
+          image.pipe(res);
+        } else {
+          res.end(screenshot, 'binary');
+        }
+      }
     }
   } catch (e) {
     const { message = '' } = e;
